db9a1b3169b1ae3baf302a4b92896e1d6ca79300
Paul Pelzl <pelzlpj@gmail.com>
2014-07-04 20:06:19 -0500

    runtime: update StringStorage against api updates in ocplib-endian 0.7

--- src/runtime/stringStorage.ml
+++ src/runtime/stringStorage.ml
@@ -48,15 +48,19 @@ let get_int16 = LittleEndian.get_int16
 let get_int32 = LittleEndian.get_int32
 let get_int64 = LittleEndian.get_int64
 
-(* Not a typo!  [LittleEndian.set_int8] "writes the least significant
-   8 bits of [v]", which is the behavior we want from [set_uint8]. *)
-let set_uint8 = LittleEndian.set_int8
-
 let set_uint32 s i v = LittleEndian.set_int32 s i (Uint32.to_int32 v)
 let set_uint64 s i v = LittleEndian.set_int64 s i (Uint64.to_int64 v)
 
 let set_int32 = LittleEndian.set_int32
 let set_int64 = LittleEndian.set_int64
+
+let set_uint8 s i v =
+  if v < 0 || v > 0xff then
+    invalid_arg "StringStorage.set_uint8"
+  else
+    (* Not a typo!  [LittleEndian.set_int8] "writes the least significant
+       8 bits of [v]", which is the behavior we want from [set_uint8]. *)
+    LittleEndian.set_int8 s i v
 
 let set_uint16 s i v =
   if v < 0 || v > 0xffff then
@@ -68,13 +72,7 @@ let set_int8 s i v =
   if v < -128 || v > 127 then
     invalid_arg "StringStorage.set_int8"
   else
-    let u8 =
-      if v < 0 then
-        (1 lsl 8) - (-v)
-      else
-        v
-    in
-    LittleEndian.set_int8 s i u8
+    LittleEndian.set_int8 s i v
 
 let set_int16 s i v =
   if v < -32768 || v > 32767 then
