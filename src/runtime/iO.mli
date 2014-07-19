66d965dd0a62cd649e3f3b40f3793c7557ef9c85
Paul Pelzl <pelzlpj@gmail.com>
2014-07-19 16:28:40 -0500

    io: functions for reading message data

--- src/runtime/iO.mli
+++ src/runtime/iO.mli
@@ -30,10 +30,12 @@
 
 type compression_t = [ `None | `Packing ]
 
+exception Unsupported_message_frame
+
 module WriteContext : sig
   type 'a t
 
-  (** [create ~write ~compression descr] creates a new write context for writing
+  (** [create ~write ~compression descr] creates a new context for writing
       data to the specified descriptor.  [compression] specifies the compression
       format, if any.
 
@@ -60,10 +62,95 @@ module WriteContext : sig
 end
 
 
+module ReadContext : sig
+  type ('a, 'stream) t
+
+  (** [create_standard ~read descr] creates a new context for reading data from the
+      specified descriptor using the standard serialization (i.e. not compressed).
+
+      The semantics of the [read] function shall mimic that of [Unix.read],
+      attempting to read into a substring of the [buf] and returning the number
+      of bytes actually read. *)
+  val create_standard : read:('a -> buf:Bytes.t -> pos:int -> len:int -> int) ->
+    'a -> ('a, Codecs.FramedStream.t) t
+
+  (** [create_packed ~read descr] creates a new context for reading data from the
+      specified descriptor using the standard packing.
+
+      The semantics of the [read] function shall mimic that of [Unix.read],
+      attempting to read into a substring of the [buf] and returning the number
+      of bytes actually read. *)
+  val create_packed : read:('a -> buf:Bytes.t -> pos:int -> len:int -> int) ->
+    'a -> ('a, Codecs.PackedStream.t) t
+
+  (** [dequeue_message context] attempts to remove a complete message from the
+      incoming queue of the read [context].  No data will be read from the
+      underlying descriptor.
+
+      @raise Unsupported_message_frame if the frame header describes a segment
+      count or segment size that is too large for the implementation *)
+  val dequeue_message : ('a, 'stream) t -> Message.rw Message.BytesMessage.Message.t option
+
+  (** [bytes_available context] obtains the number of bytes already read which
+      are currently stored in the read [context]. *)
+  val bytes_available : ('a, 'stream) t -> int
+
+  (** [read context] attempts to read some data from the underlying descriptor,
+      storing it in the read [context].
+
+      @return the number of bytes actually read *)
+  val read : ('a, 'stream) t -> int
+end
+
+
+(** [create_write_context_for_fd ~compression fd] creates a context for writing
+    messages to the given file descriptor using the specified [compression] format.
+    If [restart] is set to [true] (default), then writes failing with error
+    code Unix.EINTR will be automatically restarted. *)
+val create_write_context_for_fd : ?restart:bool -> compression:compression_t ->
+  Unix.file_descr -> Unix.file_descr WriteContext.t
+
+
+(** [create_write_context_for_channel ~compression chan] creates a context for
+    writing messages to the given buffered output channel using the specified
+    [compression] format. *)
+val create_write_context_for_channel : compression:compression_t ->
+  Pervasives.out_channel -> Pervasives.out_channel WriteContext.t
+
+
+(** [create_standard_read_context_for_fd fd] creates a context for reading messages
+    from the given file descriptor using the standard serialization (i.e.
+    not compressed).  If [restart] is set to [true] (default), then writes
+    failing with error code Unix.EINTR will be automatically restarted. *)
+val create_standard_read_context_for_fd : ?restart:bool -> Unix.file_descr ->
+  (Unix.file_descr, Codecs.FramedStream.t) ReadContext.t
+
+
+(** [create_packed_read_context_for_fd fd] creates a context for reading messages
+    from the given file descriptor using the standard packing.  If [restart]
+    is set to [true] (default), then writes failing with error code Unix.EINTR
+    will be automatically restarted. *)
+val create_standard_read_context_for_fd : ?restart:bool -> Unix.file_descr ->
+  (Unix.file_descr, Codecs.FramedStream.t) ReadContext.t
+
+
+(** [create_standard_read_context_for_channel chan] creates a context for
+    reading messages from the given input channel using the standard serialization
+    (i.e. not compressed). *)
+val create_standard_read_context_for_channel : Pervasives.in_channel ->
+  (Pervasives.in_channel, Codecs.FramedStream.t) ReadContext.t
+
+
+(** [create_packed_read_context_for_channel chan] creates a context for
+    reading messages from the given input channel using the standard packing. *)
+val create_packed_read_context_for_channel : Pervasives.in_channel ->
+  (Pervasives.in_channel, Codecs.PackedStream.t) ReadContext.t
+
+
 (** [write_message_to_fd ~compression message fd] writes the specified [message] to
     the given file descriptor, using the specified [compression] method.
     If [restart] is set to [true] (default), then writes failing with error
-    code Unix.EINTR will be automatically retried.
+    code Unix.EINTR will be automatically restarted.
 
     @raise Unix.Unix_error if a write fails *)
 val write_message_to_fd : ?restart:bool -> compression:compression_t ->
@@ -75,4 +162,43 @@ val write_message_to_fd : ?restart:bool -> compression:compression_t ->
     method. *)
 val write_message_to_channel : compression:compression_t ->
   'cap Message.BytesMessage.Message.t -> Pervasives.out_channel -> unit
+
+
+(** [read_single_message_from_fd ~compression fd] attempts to read a single
+    message from the specified file descriptor, using the given [compression]
+    method.  If [restart] is set to [true] (default), then writes failing
+    with error code Unix.EINTR will be automatically restarted.
+
+    This function is appropriate to use only when 0 or 1 messages are expected
+    to be available from the descriptor; if additional messages are available,
+    some message data could be lost.  Use [create_XXX_read_context_for_fd] to
+    correctly handle a stream of messages.
+
+    @return Some message, or None if end-of-file was reached before a message
+    could be decoded.
+
+    @raise Unix.Unix_error if a read fails
+
+    @raise Unsupported_message_frame if the frame header describes a segment
+    count or segment size that is too large for the implementation *)
+val read_single_message_from_fd : ?restart:bool -> compression:compression_t ->
+  Unix.file_descr -> Message.rw Message.BytesMessage.Message.t option
+
+
+(** [read_single_message_from_channel ~compression chan] attempts to read a single
+    message from the specified input channel, using the given [compression]
+    method.
+
+    This function is appropriate to use only when 0 or 1 messages are expected
+    to be available from the channel; if additional messages are available,
+    some message data could be lost.  Use [create_XXX_read_context_for_channel]
+    to correctly handle a stream of messages.
+
+    @return Some message, or None if end-of-file was reached before a message
+    could be decoded.
+
+    @raise Unsupported_message_frame if the frame header describes a segment
+    count or segment size that is too large for the implementation *)
+val read_single_message_from_channel : compression:compression_t ->
+  Pervasives.in_channel -> Message.rw Message.BytesMessage.Message.t option
 
