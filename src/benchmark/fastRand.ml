772b27e59ee827fb99c1cce91189e8768cca2cb4
Paul Pelzl <pelzlpj@gmail.com>
2014-07-26 12:33:39 -0500

    benchmark: switch to 128-bit xorshift rng now used in C++ bench

--- src/benchmark/fastRand.ml
+++ src/benchmark/fastRand.ml
@@ -3,9 +3,19 @@ let a = 1664525
 let c = 1013904223
 let state = ref c
 
+let x = ref 0x1d2acd47
+let y = ref 0x58ca3e14
+let z = ref 0xf563f232
+let w = ref 0x0bc76199
+
 let next () =
-  state := ((a * !state) + c) land 0xffffffff;
-   !state
+  let tmp = !x lxor (!x lsl 11) in
+  x := !y;
+  y := !z;
+  z := !w;
+  w := !w lxor (!x lsr 19) lxor tmp lxor (tmp lsr 8);
+  w := !w land 0xffffffff;
+  !w
 
 let int range =
   (next ()) mod range
